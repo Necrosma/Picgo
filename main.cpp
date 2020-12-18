c442f037ceffc8e227b6f77e3795e78070eded48
Yveltals <1302540061@qq.com>
2020-12-18 17:13:48 +0800

    .

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 int main(int argc,char** argv)
 {
-    src.open("src.txt", ios::in);
-    // src.open(argv[1], ios::in);
+    // src.open("src.txt", ios::in);
+    src.open(argv[1], ios::in);
     tokens.open("tokens.txt", ios::out);
     outFile = fopen("out.txt","wt");
     if(!src.is_open()){
